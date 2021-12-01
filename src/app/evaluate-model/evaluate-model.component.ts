73d155af3dca8fbc5ac52c38eefda477c918e7fc
Christopher Lewis <christopher_le1@nevada.unr.edu>
2021-12-01 12:48:55 -0800

    Graph working

--- src/app/evaluate-model/evaluate-model.component.ts
+++ src/app/evaluate-model/evaluate-model.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const Plotly: any;
 
 @Component({
   selector: 'app-evaluate-model',
@@ -10,6 +11,21 @@ export class EvaluateModelComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var trace1 = {
+      x: [1, 2, 3, 4],
+      y: [10, 15, 13, 17],
+      type: 'scatter'
+    };
+    
+    var trace2 = {
+      x: [1, 2, 3, 4],
+      y: [16, 5, 11, 9],
+      type: 'scatter'
+    };
+    
+    var data = [trace1, trace2];
+    
+    Plotly.newPlot('Graph', data);
   }
 
 }
