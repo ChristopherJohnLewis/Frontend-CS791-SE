fde2fdadfc5117156d700c845c94a12ef8c8b17a
Christopher Lewis <christopher_le1@nevada.unr.edu>
2021-12-09 15:32:02 -0800

    404 redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,9 +41,3 @@ export class AppComponent {
     }
   }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
